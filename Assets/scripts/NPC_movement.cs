84eb6f8e599175687a069193da0caf5fa36299db
NoahOpedal <nopedal@gmail.com>
2024-10-28 10:54:15 +0100

    charracter: update

--- Assets/scripts/NPC_movement.cs
+++ Assets/scripts/NPC_movement.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class NPC_movement : MonoBehaviour
 {
+    public Rigidbody2D NPC_rigidbody
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame

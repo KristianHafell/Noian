3d05298245a32f6390b06c7816e8277a44f448e7
NoahOpedal <nopedal@gmail.com>
2024-10-28 11:40:43 +0100

    NPC: NPC now has the ability to find a random direction. This will later be used for movement.

--- Assets/scripts/NPC_movement.cs
+++ Assets/scripts/NPC_movement.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System;
 
 public class NPC_movement : MonoBehaviour
 {
-    public Rigidbody2D NPC_rigidbody
+    public Rigidbody npcRigidbody;
+    public int speed = 5;
+    private Vector3[] directions = {Vector3.right, Vector3.left, Vector3.forward, Vector3.back};
+    private System.Random random = new System.Random();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Generate random direction
+        int randomNumber = random.Next(0,4);
+        Vector3 randomDirection = directions[randomNumber];
+        Debug.Log(randomNumber);
+        Debug.Log(randomDirection);
     }
 }

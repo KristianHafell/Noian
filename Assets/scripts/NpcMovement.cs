f7e02bdb679024291ec35adf60526ee8ea97e0d9
NoahOpedal <nopedal@gmail.com>
2024-10-28 12:14:27 +0100

    NPC: calculates distance between self and house, door

--- Assets/scripts/NpcMovement.cs
+++ Assets/scripts/NpcMovement.cs
@@ -5,9 +5,13 @@ using System;
 public class NPC_movement : MonoBehaviour
 {
     public Rigidbody npcRigidbody;
+    public BoxCollider houseBoxCollider;
+    public BoxCollider doorBoxCollider;
+
     public int speed = 5;
     private Vector3[] directions = {Vector3.right, Vector3.left, Vector3.forward, Vector3.back};
     private System.Random random = new System.Random();
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,10 +22,16 @@ public class NPC_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Update distance between NPC and its house and door
+        Vector3 npcPosition = npcRigidbody.position;
+        Vector3 closestPointHouse = houseBoxCollider.ClosestPoint(npcPosition);
+        Vector3 closestPointDoor = doorBoxCollider.ClosestPoint(npcPosition);
+
+        float distanceHouse = Vector3.Distance(npcPosition, closestPointHouse);
+        float distanceDoor = Vector3.Distance(npcPosition, closestPointDoor);
+
         // Generate random direction
         int randomNumber = random.Next(0,4);
         Vector3 randomDirection = directions[randomNumber];
-        Debug.Log(randomNumber);
-        Debug.Log(randomDirection);
     }
 }
